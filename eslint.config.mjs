e496267ff45efccbd00ee93b3cc09f5267198ace
Bedirhan SAY <bedirhan.sayy@gmail.com>
2025-06-15 18:23:48 +0300

    fix(projects): initials

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+    },
+  }
+);
