9af11f811058932ecff973c3bce95955d1c14266
Bedirhan SAY <bedirhan.sayy@gmail.com>
2025-06-16 13:35:47 +0300

    fix(projects): esModule

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const platform_express_1 = require("@nestjs/platform-express");
-const express = require("express");
+const express_1 = __importDefault(require("express"));
 const app_module_1 = require("./app.module");
-const server = express();
+const server = (0, express_1.default)();
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(server));
     await app.init();
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 import { AppModule } from './app.module';
 
 const server = express();
