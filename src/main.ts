4b23651a239555f1aaa6815d92efffce6445c93f
Bedirhan SAY <bedirhan.sayy@gmail.com>
2025-06-16 19:10:33 +0300

    fix(projects): fix defect

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: '*', // ⚠️ Geliştirme aşamasında * kullan, üretimde domain belirt
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-company-id'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
