d91e0eac56fd8ee11107c75a7b1a9a8e2df68632
Bedirhan SAY <bedirhan.sayy@gmail.com>
2025-06-16 15:41:55 +0300

    fix(projects): fix defect

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,9 @@
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
 import { AppModule } from './app.module';
 
-const server = express();
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.init();
-
-  server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-  });
+  await app.listen(3000);
 }
-
 bootstrap();
-
-export default server;
