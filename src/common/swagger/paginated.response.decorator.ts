da8f3326c684283d29410f3eff9f4313fac1c7f8
Bedirhan SAY <bedirhan.sayy@gmail.com>
2025-06-19 12:15:04 +0300

    fix(projects): response fix

--- src/common/swagger/paginated.response.decorator.ts
+++ src/common/swagger/paginated.response.decorator.ts
@@ -3,24 +3,29 @@ import { applyDecorators, Type } from '@nestjs/common';
 import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
 import { PaginatedResponseDto } from '../DTO/response';
 
-export const ApiPaginatedResponse = <TModel extends Type<unknown>>(model: TModel, description = 'Liste getirildi') =>
+export const ApiPaginatedResponse = <TModel extends Type<unknown>>(model: TModel) =>
   applyDecorators(
     ApiExtraModels(PaginatedResponseDto, model),
     ApiOkResponse({
-      description,
       schema: {
-        type: 'object',
-        properties: {
-          items: {
-            type: 'array',
-            items: { $ref: getSchemaPath(model) },
+        allOf: [
+          {
+            $ref: getSchemaPath(PaginatedResponseDto),
           },
-          pageNumber: { type: 'number', example: 1 },
-          totalPages: { type: 'number', example: 5 },
-          totalCount: { type: 'number', example: 50 },
-          hasPreviousPage: { type: 'boolean', example: false },
-          hasNextPage: { type: 'boolean', example: true },
-        },
+          {
+            properties: {
+              items: {
+                type: 'array',
+                items: { $ref: getSchemaPath(model) },
+              },
+              pageNumber: { type: 'number', example: 1 },
+              totalPages: { type: 'number', example: 5 },
+              totalCount: { type: 'number', example: 50 },
+              hasPreviousPage: { type: 'boolean', example: false },
+              hasNextPage: { type: 'boolean', example: true },
+            },
+          },
+        ],
       },
     })
   );
