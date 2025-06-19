6caf70dfc0da662160c3fce35aa0dda89427375d
Bedirhan SAY <bedirhan.sayy@gmail.com>
2025-06-19 11:38:23 +0300

    fix(projects): response fix

--- src/common/swagger/apply-query.ts
+++ src/common/swagger/apply-query.ts
@@ -3,10 +3,10 @@ import { ApiQuery } from '@nestjs/swagger';
 
 export function ApiPaginatedQuery() {
   return applyDecorators(
-    ApiQuery({ name: 'page', required: true, description: 'Sayfa numarası', type: Number }),
+    ApiQuery({ name: 'pageNumber', required: true, description: 'Sayfa numarası', type: Number }),
     ApiQuery({ name: 'pageSize', required: true, description: 'Sayfa başına kayıt sayısı', type: Number }),
     ApiQuery({ name: 'search', required: false, description: 'İsim ile arama yapılır', type: String }),
-    ApiQuery({ name: 'beginDate', required: false, description: 'Başlangıç tarihi', type: String }),
-    ApiQuery({ name: 'endDate', required: false, description: 'Bitiş tarihi', type: String })
+    ApiQuery({ name: 'beginDate', required: false, description: 'Başlangıç tarihi (ISO formatında)', type: String }),
+    ApiQuery({ name: 'endDate', required: false, description: 'Bitiş tarihi (ISO formatında)', type: String })
   );
 }
